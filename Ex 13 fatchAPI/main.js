3915bb9d86de60585e0292f2d3324e0332bbf3ef
Vivek <vivek.munjapara@gmail.com>
2022-12-02 12:25:36 +0530

    fetchAPI

---
+++ Ex 13 fatchAPI/main.js
@@ -0,0 +1,9 @@
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((data) => data.json())
+  .then((data) => display(data));
+
+function display(data) {
+  document.getElementById('tbl').innerHTML = data.map((value) => {
+    return `<tr><td>${value.userId}</td><td>${value.id}</td><td>${value.title}</td><td>${value.body}</td></tr>`;
+  }).join("");
+}
