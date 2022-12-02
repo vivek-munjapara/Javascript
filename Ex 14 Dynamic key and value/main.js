756319d1a866a640f74b400b785126575d28b8f1
Vivek <vivek.munjapara@gmail.com>
2022-12-02 15:01:41 +0530

    commit

---
+++ Ex 14 Dynamic key and value/main.js
@@ -0,0 +1,29 @@
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((y) => y.json())
+  .then((y) => display(y));
+
+
+function display(y) {
+  let tbody = y.map((value) => {
+    let trow = "";
+    let obj1 = {};
+    for (const key in value) {
+      obj1 = value[key]
+      if ( typeof obj1 === 'object') {
+        
+        for (const key2 in obj1) {
+          trow+=`<td>${obj1[key2]}</td>`
+          
+        }
+
+      }
+      else {
+        trow+=`<td>${value[key]}</td>`
+      }
+    
+    }
+    
+    return `<tr> ${trow}</tr>`;
+  });
+  document.getElementById("tbl").innerHTML = tbody.join(" ");
+}
