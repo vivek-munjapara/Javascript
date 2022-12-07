bac67a78f9b6c42bc4c1d156369b891d68c54114
Vivek <vivek.munjapara@gmail.com>
2022-12-07 16:55:11 +0530

    DONE

--- Ex 19 Form marks update/main.js
+++ Ex 19 Form marks update/main.js
@@ -5,11 +5,37 @@ add = () => {
 
     document.getElementById("dsply").innerHTML +=
         `<div> ${fname}
-        <input type="text" value=" ${marks} " >
+        <input type="text" value=" ${marks} " class="marks" >
         <input type="checkbox">
             <button onclick="incremarks(this)"> + </button>
             <button onclick="decremarks(this)"> - </button>
         </div>`
+}
+
+changemarks = () => {
+    let chngmarks = document.getElementById("updatedmarks").value;
+
+    let a = document.querySelectorAll("input:checked").length;
+    let allmarkschng = document.querySelectorAll(".marks");
+
+    // console.log(a);
+    if (a == 0) {
+        for (const iterator of allmarkschng) {
+            iterator.value = chngmarks;
+        }
+    } else {
+        for (const iterator of allmarkschng) {
+
+            // console.log(iterator);
+            if (iterator.parentElement.querySelector("input:checked")) {
+                iterator.value = chngmarks;
+
+
+            }
+        }
+    }
+
+
 }
 
 
