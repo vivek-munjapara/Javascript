b647bd62d124b4a4287feb28a5541122a9691cb2
Vivek <vivek.munjapara@gmail.com>
2022-12-13 16:49:25 +0530

    commit

--- Ex 22 post API form/main.js
+++ Ex 22 post API form/main.js
@@ -1,24 +1,23 @@
-let frm = document.forms["frm"];
+let formitem = document.forms["frm"];
 let frminput = [];
 
 getvalue = () => {
+    // console.log(formitem[0]);
+
     let val = {};
+    let validate = true;
+
     let i = 0;
 
     for (const iterator of frm) {
         val[iterator.name] = iterator.value;
-        
+
     }
 
 
-    // validation  
-
-    let validate = true;
-
-    for (const iterator of frm) {
+    for (const iterator of formitem) {
         // console.log(iterator);
         document.getElementsByClassName("error")[i].innerHTML = "";
-
         if (iterator.value.length == 0) {
             error(i, "Please enter a Value");
             validate = false;
@@ -27,13 +26,15 @@ getvalue = () => {
             if (!Number.isInteger(parseInt(iterator.value))) {
                 error(i, "Please Number Only");
                 validate = false;
+
             }
         } else if (iterator.name == "email") {
             if (iterator.value.indexOf("@") == -1) {
                 error(i, "Please valid email id");
                 validate = false;
+
             }
-        } 
+        }
         i += 1;
 
     }
@@ -44,8 +45,6 @@ getvalue = () => {
     }
 
 
-
-    // console.log(val);
 }
 
 error = (id, error) => {
