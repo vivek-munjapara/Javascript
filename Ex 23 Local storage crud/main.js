4d49ac9cb9804d12bb69d60af0bbb10eb54a8333
Vivek <vivek.munjapara@gmail.com>
2022-12-15 13:50:51 +0530

    local storage commit

--- Ex 23 Local storage crud/main.js
+++ Ex 23 Local storage crud/main.js
@@ -1,21 +1,60 @@
-
+let data = '';
+let editName = -1;
 
 submit = () => {
 
-    if (localStorage.getItem("First Name") == null) {
-
-        localStorage.setItem("First Name", document.getElementById("fname").value);
-
+    if (document.getElementById("fname").value == "") {
+        alert("Please input Name");
     } else {
 
-        let existingData = localStorage.getItem("First Name").split(",");
-        
-        existingData.push(document.getElementById("fname").value);
+        if (localStorage.getItem("FirstName") == null) {
 
-        localStorage.setItem("First Name", existingData.toString());
-        // console.log(existingData);
+            localStorage.setItem("FirstName", document.getElementById("fname").value);
 
+        } else {
+
+            let existingData = localStorage.getItem("FirstName").split(",");
+            if (editName != -1) {
+                
+                existingData[editName] = document.getElementById("fname").value;
+                
+                localStorage.setItem("FirstName", existingData.toString());
+
+            } else {
+
+                existingData.push(document.getElementById("fname").value);
+
+                localStorage.setItem("FirstName", existingData.toString());
+                // console.log(existingData);
+            }
+            editName = -1;
+        }
     }
+    displayData();
+}
+displayData = () => {
 
+     data = localStorage.getItem("FirstName").split(",");
 
+    document.getElementById("dis").innerHTML = data.map((element, index) => {
+        return `<div><p>${element}</p><button onclick="editData(${index})">Edit</button><button onclick="dltData(${index})">Delete</button></div>`
+    }).join("");
+
+    // console.log(data);
+}
+
+dltData = (index) => {
+    // console.log(index);
+    data.splice(index, 1);
+   
+    localStorage.setItem("FirstName", data.toString());
+    
+    displayData();
+}
+
+editData = (index) => { 
+    
+    editName = index;
+
+    document.getElementById("fname").value = data[index];
 }
