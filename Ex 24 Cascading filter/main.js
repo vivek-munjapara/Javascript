d40f97c04e974ca25394162e957b8c4d9548522a
Vivek <vivek.munjapara@gmail.com>
2022-12-15 16:25:28 +0530

    cascading commit

--- Ex 24 Cascading filter/main.js
+++ Ex 24 Cascading filter/main.js
@@ -1,21 +1,71 @@
+var mydata = [{
+
+    countyName: 'India',
+    state: [{
+        stateName: "gujrat",
+        city: ["vadodara", "anand", "rajkot"]
+    },
+    {
+        stateName: "mahrastra",
+        city: ["pune", "mumbai"]
+    }]
+
+},
+
+{
+
+    countyName: 'Canada',
+    state: [{
+        stateName: "Ontario",
+        city: ["Toronto", "Bramtomp"]
+    },
+    {
+        stateName: "Alberta",
+        city: ["Calgary", "Camrose","Chestermere"]
+    }]
+
+}];
 
 
-submit = () => {
 
-    if (localStorage.getItem("First Name") == null) {
+selectCountry = () => {
+    document.getElementById("country").innerHTML = mydata.map((element) => {
+        return `<option value="${element.countyName}">${element.countyName}</option>`
+    }).join("");
 
-        localStorage.setItem("First Name", document.getElementById("fname").value);
-
-    } else {
-
-        let existingData = localStorage.getItem("First Name").split(",");
-        
-        existingData.push(document.getElementById("fname").value);
-
-        localStorage.setItem("First Name", existingData.toString());
-        // console.log(existingData);
-
-    }
-
-
+    document.getElementById("country").onchange();
+    document.getElementById("state").onchange();
 }
+
+selectState = () => {
+    let country = document.getElementById("country").value;
+    
+    let state = mydata.find((element) => {
+        // console.log(element);
+        return element.countyName == country;
+        
+    }).state.map((value) => {
+        return `<option value="${value.stateName}">${value.stateName}</option>`
+        
+    }).join("");
+    
+    document.getElementById("state").innerHTML = state;
+};
+
+selectCity = () => {
+    let country = document.getElementById("country").value;
+    let state = document.getElementById("state").value;
+    
+    let city1 = mydata.find((ctry) => {
+        return ctry.countyName == country;
+        
+    }).state.find((st) => {
+        return st.stateName == state;
+    }).city.map((value) => {
+        return `<option value="${value}">${value}</option>`
+    }).join("");
+    
+    // console.log(city);
+    document.getElementById("city").innerHTML = city1;
+}
+selectCountry(); 
