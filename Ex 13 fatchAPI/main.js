5aa160edfc07d2cc43242c9e103ebfbc77d22b81
Vivek <vivek.munjapara@gmail.com>
2022-12-16 18:07:24 +0530

    half commit

--- Ex 13 fatchAPI/main.js
+++ Ex 13 fatchAPI/main.js
@@ -3,9 +3,12 @@ fetch("https://jsonplaceholder.typicode.com/posts")
   .then((data) => display(data));
 
 function display(data) {
-  document.getElementById("tbl").innerHTML = data
-    .map((value) => {
-      return `<tr><td>${value.userId}</td><td>${value.id}</td><td>${value.title}</td><td>${value.body}</td></tr>`;
+  document.getElementById("tbl").innerHTML = data.map((value) => {
+      return `<tr><td>${value.userId}</td>
+                  <td>${value.id}</td>
+                  <td>${value.title}</td>
+                  <td>${value.body}</td>
+                </tr>`;
     })
     .join("");
 }
