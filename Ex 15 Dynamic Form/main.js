20d61f103b165ebfb16853ca0fa7c68aa2bbd6ee
Vivek <vivek.munjapara@gmail.com>
2022-12-19 18:48:40 +0530

    commit all done

--- Ex 15 Dynamic Form/main.js
+++ Ex 15 Dynamic Form/main.js
@@ -1,5 +1,43 @@
 let arry = [];
 let data = document.getElementsByTagName("input");
+let forminpt = document.forms["form"];
+
+let validate = true;
+// console.log(forminpt);
+
+
+
+validation = () => {
+  // console.log(formitem[0]);
+  let i = 0;
+
+  for (const iterator of forminpt) {
+    // console.log(iterator);
+    document.getElementsByClassName("error")[i].innerHTML = "";
+    if (iterator.value.length == 0) {
+      error(i, "Please enter a Value");
+      validate = false;
+    }
+    else if (iterator.name == "mobileNumber") {
+      if (!Number.isInteger(parseInt(iterator.value))) {
+        error(i, "Please Number Only");
+        validate = false;
+      }
+    } else if (iterator.name == "email") {
+      if (iterator.value.indexOf("@") == -1) {
+        error(i, "Please valid email id");
+        validate = false;
+      }
+    }
+    i += 1;
+
+  }
+}
+
+error = (id, error) => {
+  // console.log(id + error);
+  document.getElementsByClassName("error")[id].innerHTML = error;
+}
 
 
 function submit() {
@@ -9,15 +47,20 @@ function submit() {
     ob[data[key].name] = data[key].value;
   }
 
+  if (validation != false) {
     arry.push(ob);
-    
-    document.getElementById('tbl').innerHTML = arry.map(function (value) {
-        return `<tr>
+  }
+
+  document.getElementById('tbl').innerHTML = arry.map(function (value) {
+    return `<tr>
                     <td>${value.firstName}</td>
                     <td>${value.lastName}</td>
                     <td>${value.mobileNumber}</td>
                     <td>${value.email}</td>
                 </tr>`;
 
-    }).join("");
+  }).join("");
+
+
+  validation();
 }
