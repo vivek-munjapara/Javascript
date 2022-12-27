63520d43ca9cab6014f304f781d15b8ffb40b09e
Vivek <vivek.munjapara@gmail.com>
2022-12-27 17:54:18 +0530

    reverse watch commit

--- Ex 25 Watch/main.js
+++ Ex 25 Watch/main.js
@@ -4,3 +4,34 @@ timeNew = () => {
     }, 1000)
 }
 timeNew();
+
+
+reverseCountdown = () => {
+
+    setInterval(() => {
+        let endDate = new Date("2022-12-31 00:00:00");
+        // console.log(endDate)
+        let currentDate = new Date();
+        let remainingTime = endDate-currentDate;
+
+        var days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+
+        document.getElementById("time2").innerHTML = days +`Day `
+            + hours + `:` + minutes + `:` + seconds;
+        
+        
+        if (remainingTime < 0) {
+            clearInterval();
+            document.getElementById("time2").innerHTML = "EXPIRED";
+        }
+        
+        
+        
+    }, 1000)
+
+}
+
+reverseCountdown();
